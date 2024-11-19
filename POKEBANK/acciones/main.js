03a07a4b14787c0c6bf88780853dd0a8dd55326a
Ulma Escobar <68133338+ulmae@users.noreply.github.com>
2024-11-19 08:45:29 -0600

    Update main.js
    Cambio que no se habia subido en el main de las acciones

--- POKEBANK/acciones/main.js
+++ POKEBANK/acciones/main.js
@@ -1,15 +1,30 @@
-const username = "Ulma";
-const accountNumber = "0156620";
-const saldo = 200;
-document.getElementById("username").innerText = `${username}`;
-document.getElementById("account-number").innerText = `${accountNumber}`;
+var historial = [];
+var usuario = JSON.parse(localStorage.getItem("usuario")) || {
+    nombre: "Ash Ketchum",
+    cuenta: "0987654321",
+    saldo: 500.0,
+  };
+document.getElementById("username").innerText = `${usuario.nombre}`;
+document.getElementById("account-number").innerText = `${usuario.cuenta}`;
 
 function deposito() {
-    //TODO: Validacion de montos maximos/minimos
     swal("Monto a depositar:", {
         content: "input",
     }).then((value)=>{
-        if((typeof Number(value) === 'number') && !isNaN(value)){
+        var number = parseInt(value);
+        if((typeof Number(value) === 'number') && !isNaN(number)){
+            usuario.saldo = parseInt(usuario.saldo) + number;
+            localStorage.setItem("usuario", JSON.stringify(usuario));
+            var today = new Date();
+            const transaction = {
+                date: today,
+                description: "Depósito",
+                category: "Depósito",
+                amount: number,
+                balance: saldo + number
+            }
+            historial.push(transaction);
+            localStorage.setItem("historial", JSON.stringify(usuario));
             swal(`Depositaste $ ${value}`)
         } else {
             swal(`El valor ingresado no es un número. Intente nuevamente.`)
@@ -18,12 +33,16 @@ function deposito() {
     })
 }
 function retiro() {
-    //TODO: Validacion de montos maximos/minimos
     swal("Monto a retirar:", {
         content: "input",
     }).then((value)=>{
-        if((typeof Number(value) === 'number') && !isNaN(value)){
-            swal(`Se han retirado $ ${value}`)
+        var number = parseInt(value);
+        if((typeof Number(value) === 'number') && !isNaN(value) && number<=usuario.saldo){
+            usuario.saldo = parseInt(usuario.saldo) - number;
+            localStorage.setItem("usuario", JSON.stringify(usuario));
+            swal(`Se han retirado $ ${number}`)
+        } else if(number>parseInt(usuario.saldo)) {
+            swal(`El monto a retirar excede su saldo actual.`)
         } else {
             swal(`El valor ingresado no es un número. Intente nuevamente.`)
         }
@@ -31,18 +50,23 @@ function retiro() {
     })
 }
 function consulta() {
-    swal(`Su saldo actual es de $${saldo}`);
+    swal(`Su saldo actual es de $${usuario.saldo}`);
 }
 function pago() {
     swal("Ingrese el NPE del servicio a pagar:", {
         content: "input",
     }).then((value)=>{
+        var number = parseInt(value);
         if((typeof Number(value) === 'number') && !isNaN(value)){
             swal(`Ingrese el monto a abonar al servicio:`, {
                 content: "input"
             }).then((payment)=>{
-                if((typeof Number(value) === 'number') && !isNaN(value)) {
+                if((typeof Number(value) === 'number') && !isNaN(number) && number<=parseInt(usuario.saldo)) {
+                    usuario.saldo = parseInt(usuario.saldo) - number;
+                    localStorage.setItem("usuario", JSON.stringify(usuario));
                     swal(`Se han abonado $ ${payment}`)
+                } else if (number>parseInt(usuario.saldo)){
+                    swal(`El monto a abonar excede su saldo actual.`)
                 } else {
                     swal(`El valor ingresado no es un número. Intente nuevamente.`)
                 }
